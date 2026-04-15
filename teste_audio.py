d2aafab1bd74aaa0f4ca37a3802c1d9814397809
jonatttttttan <jonathan.iuliany2@gmail.com>
2026-04-15 00:31:11 -0300

    teste

--- teste_audio.py
+++ teste_audio.py
@@ -8,6 +8,7 @@ load_dotenv()
 client = OpenAI()
 texto = "Olá mundo, este áudio foi gerado pela API da OPNEAI"
 
+# testando áudio
 with client.audio.speech.with_streaming_response.create(
     model="gpt-4o-mini-tts",
     voice="nova",
